4b82eca5efada611eba9cc58c802d175427c6e16
billygrahan <billygrahan09@gmail.com>
2025-11-05 13:28:38 -0300

    ajuste bfs

--- Algoritmos/BFS.py
+++ Algoritmos/BFS.py
@@ -5,74 +5,75 @@ import random
 import time
 import sys
 
-# Busca em Largura (BFS)
+# Busca em Largura Modificada (BFS)
+# Explora TODOS os caminhos possíveis até a fila esvaziar
+# Sempre atualiza quando encontra um caminho com MENOR CUSTO TOTAL
 
 def bfs(graph_distance, origem, destino):
-    cores = {}
+    """
+    BFS que explora TODOS os caminhos até a fila esvaziar.
+    Sempre que encontra o destino, verifica se o custo é menor
+    que o melhor caminho encontrado anteriormente.
+    """
     distancias = {}
     predecessores = {}
     
     quantidade_nos_expandidos_bfs = 0
     quantidade_filhos_bfs = 0
     
-    # Inicialização dos dicionários com valores padrão
+    # Inicialização: todas as distâncias começam como infinito
     for vertice in graph_distance.keys():
-        cores[vertice] = 'Branco'
         distancias[vertice] = float('inf')
         predecessores[vertice] = None
     
-    # Altera os valores do primeiro vertice
-    cores[origem] = 'Cinza'
+    # A distância da origem para ela mesma é 0
     distancias[origem] = 0
-    predecessores[origem] = None
-
-    fila_vertices = []
-    fila_vertices.append(origem)
-
-    # Inicia o BFS
-    while fila_vertices != []:
-        # Remove o primeiro vértice da fila
-        desenfileira = fila_vertices.pop(0)
+    
+    # Fila: armazena (distância_acumulada, vértice_atual)
+    fila_vertices = [(0, origem)]
+    
+    # ✅ EXPLORA TODOS OS CAMINHOS - NÃO PARA AO ENCONTRAR O DESTINO
+    while fila_vertices:
+        # Remove o primeiro da fila
+        distancia_atual, vertice_atual = fila_vertices.pop(0)
         
         quantidade_nos_expandidos_bfs += 1
-
-        # Verifica se encontrou o destino
-        if desenfileira == destino:
-            break
-
-        numero_filhos_do_vertice_bfs = 0
-
-        # Loop com os vértices adjacentes do vértice desenfileirado
-        for vertice in graph_distance[desenfileira].keys():
-            # Verifica se o vértice não foi visitado
-            if cores[vertice] == 'Branco':
-                # Marca o vértice como visitado
-                cores[vertice] = 'Cinza'
-                distancias[vertice] = distancias[desenfileira] + 1
-                predecessores[vertice] = desenfileira
-                fila_vertices.append(vertice)
-                numero_filhos_do_vertice_bfs += 1
-
-         # Atualiza o contador de filhos do BFS
-        quantidade_filhos_bfs += numero_filhos_do_vertice_bfs
         
-        # O vértice e todos os seus vizinhos já foram visitados
-        cores[desenfileira] = 'Preto'
-
+        # NÃO PARA AQUI - continua explorando mesmo se for o destino
+        # Se a distância atual já é maior que a melhor conhecida, pula
+        # if distancia_atual > distancias[vertice_atual]:
+        #     continue
+        
+        numero_filhos_do_vertice_bfs = 0
+        
+        # Explora todos os vizinhos
+        for vizinho, peso_aresta in graph_distance[vertice_atual].items():
+            # Calcula a nova distância até o vizinho
+            nova_distancia = distancia_atual + peso_aresta
+            
+            # ✅ ATUALIZA se encontrou um caminho MELHOR (menor custo)
+            if nova_distancia < distancias[vizinho]:
+                distancias[vizinho] = nova_distancia
+                predecessores[vizinho] = vertice_atual
+                
+                # Adiciona o vizinho na fila para explorar seus vizinhos
+                fila_vertices.append((nova_distancia, vizinho))
+                numero_filhos_do_vertice_bfs += 1
+        
+        quantidade_filhos_bfs += numero_filhos_do_vertice_bfs
+    
+    # ✅ Após explorar TODOS os caminhos, reconstrói o melhor caminho encontrado
     rota = []
     caminho_atual = destino
-
-    # Começa a procurar a rota
-    while caminho_atual != origem:
-        caminho_atual = predecessores[caminho_atual]
+    
+    # Reconstrói o caminho do destino até a origem
+    while caminho_atual is not None:
         rota.append(caminho_atual)
-
+        caminho_atual = predecessores[caminho_atual]
+    
     rota.reverse()
-    rota.append(destino)
-
-    # Calcula a distância total do caminho
-    distancia_total = 0
-    for i in range(len(rota) - 1):
-        distancia_total += graph_distance[rota[i]][rota[i+1]]
-
+    
+    # A distância total já está calculada em distancias[destino]
+    distancia_total = distancias[destino]
+    
     return rota, distancia_total, quantidade_nos_expandidos_bfs, quantidade_filhos_bfs
